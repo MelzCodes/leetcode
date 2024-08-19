50dd3a1ea88fb6ea3f13ce0bcfba896c1a0deb4e
Matteo <117614040+MelzCodes@users.noreply.github.com>
2024-08-19 15:21:19 +0100

    Time: 17 ms (10.22%) | Memory: 45.5 MB (6.01%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -2,27 +2,44 @@ class Solution {
     public String reverseWords(String s) {
         s = s.trim();
         StringBuilder ans = new StringBuilder();
-        LinkedHashMap<Integer, String> hashy = new LinkedHashMap<>();
-        StringBuilder currentWord = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == ' ') {
-                hashy.put(i, currentWord.toString());
-                currentWord.setLength(0);
-            } else {
-                currentWord.append(c);
-                if (i == s.length()-1) hashy.put(i, currentWord.toString());
-            } 
+        int p1 = 0;
+        int p2 = s.length()-1;
+        StringBuilder p1Word = new StringBuilder();
+        StringBuilder p2Word = new StringBuilder();
+        ArrayList<String> p1Words = new ArrayList<>();
+        ArrayList<String> p2Words = new ArrayList<>();
+        int index = 0;
+        while (p1 <= p2) {
+            while (index != s.length() && s.charAt(p1) != ' ' && p1 <= p2) {
+                p1Word.append(s.charAt(p1));
+                p1++;
+                index++;
+            }
+            while (s.charAt(p2) != ' ' && p1 <= p2) {
+                p2Word.append(s.charAt(p2));
+                p2--;
+            }
+            p1Words.add(p1Word.toString().trim());
+            p2Words.add(reverseString(p2Word.toString().trim()));
+            p1Word.setLength(0);
+            p2Word.setLength(0);
+            p1++;
+            p2--;
         }
-
-        Object[] words = hashy.values().toArray();
-        for (int i = words.length-1; i>=0; i--) {
-            String word = words[i].toString().trim();
-            if (word.length() == 0) continue;
-            if (i == 0) ans.append(word);
-            else ans.append(word + " ");
+        for (String str : p2Words) {
+            if (str.length() == 0) continue;
+            ans.append(str + " ");
+        }
+        for (int i = p1Words.size()-1; i >= 0; i--) {
+            if (p1Words.get(i).length() == 0) continue;
+            if (i == 0) ans.append(p1Words.get(i));
+            else ans.append(p1Words.get(i) + " ");
         }
         return ans.toString();
+    }
+
+    private String reverseString(String s) {
+        if (s.length() == 0) return s;
+        return (reverseString(s.substring(1)) + s.charAt(0));
     }
 }
