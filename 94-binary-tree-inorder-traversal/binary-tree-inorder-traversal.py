0a33553606972722dde9931ca794fb7445ab7356
Matteo <117614040+MelzCodes@users.noreply.github.com>
2025-01-08 19:42:44 +0000

    Time: 0 ms (100.00%) | Memory: 17.9 MB (12.60%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -5,7 +5,6 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    path = []
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         path = []
         def dfs(root):
