5192dbdcef808a4691e70d3308788ecabd5505fa
Matteo <117614040+MelzCodes@users.noreply.github.com>
2025-01-09 11:44:53 +0000

    Time: 0 ms (100.00%) | Memory: 21.1 MB (27.55%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -5,23 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    res = None
+    k = None
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        count = 0
-        ans = 0
-        def dfs(root):
-            if not root:
-                return
-            
-            dfs(root.left)
-            nonlocal count
-            count += 1
-            if count == k:
-                nonlocal ans
-                ans = root.val
-                return
-            dfs(root.right)
+        self.k = k
+        self.inorder(root)
+        return self.res
+    
+    def inorder(self, root):
+        if root is None:
+            return
+        if self.res is not None:
+            return
+        
+        self.inorder(root.left)
+ 
+        self.k -= 1
+        if self.k == 0:
+            self.res = root.val
             return
 
-        dfs(root)
-        return ans
-        
+        self.inorder(root.right)
